ceb76cc83f00da234edbd38437fdf53ec404a32b
Guido <ppapaguido@gmail.com>
2021-11-09 14:43:46 +0100

    aggiunta variabile fiz e buz come 'a' e 'b'

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const cont = document.querySelector('.container');
 
+const a = ('fizz');
+const b = ('buz');
+
 for (let i = 1; i <= 50; i++) {
     if ( i % 3 ==  0 && i % 5 == 0){
-    cont.innerHTML += `<div class = "box red">fizzbuzz </div>`
+    cont.innerHTML += `<div class = "box red">${a + b }</div>`
     }
     else if ( i % 3 == 0 ){
-    cont.innerHTML += `<div class = "box green">fizz</div>`
+    cont.innerHTML += `<div class = "box green">${a}</div>`
     } else if (i % 5 == 0) {
-    cont.innerHTML += `<div class = "box yellow">buzz</div>`
+    cont.innerHTML += `<div class = "box yellow">${b}</div>`
     }
     else {
     cont.innerHTML += `<div class = "box blue">${i}</div>`;
