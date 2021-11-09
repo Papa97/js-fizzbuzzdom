b397cb6b12b8036a19a3abaa03626a1c5334e638
Guido <ppapaguido@gmail.com>
2021-11-09 15:43:36 +0100

    correzioni js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const cont = document.querySelector('.container');
 
-const a = ('fizz');
-const b = ('buz');
+const firstWord = 'fizz';
+const secodnWord = 'buz';
 
 for (let i = 1; i <= 50; i++) {
     if ( i % 3 ==  0 && i % 5 == 0){
-    cont.innerHTML += `<div class = "box red">${a + b }</div>`
+    cont.innerHTML += `<div class = "box red">${firstWord + secodnWord }</div>`
     }
     else if ( i % 3 == 0 ){
-    cont.innerHTML += `<div class = "box green">${a}</div>`
+    cont.innerHTML += `<div class = "box green">${firstWord}</div>`
     } else if (i % 5 == 0) {
-    cont.innerHTML += `<div class = "box yellow">${b}</div>`
+    cont.innerHTML += `<div class = "box yellow">${secodnWord}</div>`
     }
     else {
     cont.innerHTML += `<div class = "box blue">${i}</div>`;
